9ad0a46730dffbc2ec87f18fbe15ddbbfec2aa99
Michael Josephson <mike@josephson.org>
2016-01-21 17:51:34 +0000

    filter consecutive plays of the same track from last.fm

--- scripts/sources.py
+++ scripts/sources.py
@@ -6,6 +6,9 @@ import cgi
 from datetime import date, datetime, timedelta
 from xml.parsers.expat import ExpatError
 
+from itertools import imap, groupby, islice
+from operator import itemgetter
+
 import pytz
 import feedparser # http://feedparser.org/
 from icalendar import Calendar
@@ -20,15 +23,19 @@ def get_last_fm(user, api_key):
     if doc.firstChild.nodeName == 'lfm' and \
             doc.firstChild.getAttribute('status') == 'ok':
         tracks = doc.getElementsByTagName('track')
-        items = []
-        for track in tracks[:15]:
-            artist = track.getElementsByTagName('artist')[0].firstChild.nodeValue
-            name = track.getElementsByTagName('name')[0].firstChild.nodeValue
-            url = track.getElementsByTagName('url')[0].firstChild.nodeValue
-            items.append(artist + " - " + make_link(name, url))
-        return make_ul(items)
+        return make_ul(islice(strip_consecutive_duplicates([last_fm_track_link(track) for track in tracks]), 15))
     else:
         raise Exception('Unexpected response from last.fm: %s' % xml_string)
+
+def last_fm_track_link(track):
+    artist = track.getElementsByTagName('artist')[0].firstChild.nodeValue
+    name = track.getElementsByTagName('name')[0].firstChild.nodeValue
+    url = track.getElementsByTagName('url')[0].firstChild.nodeValue
+    return artist + " - " + make_link(name, url)
+
+# from http://stackoverflow.com/a/3463410/63034
+def strip_consecutive_duplicates(iterable, key=None):
+    return imap(next, imap(itemgetter(1), groupby(iterable, key)))
 
 def get_bookmarks(end_point, user = None, password = None, auth_token = None):
     if auth_token:
