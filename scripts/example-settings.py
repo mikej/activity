d79fc41b259941657ef9ca93366ad143ecfe0c2f
Michael Josephson <mike@josephson.org>
2016-11-20 14:12:21 +0000

    update exampl settings
    update example settings following breaking sources into separate modules

--- scripts/example-settings.py
+++ scripts/example-settings.py
@@ -2,12 +2,12 @@ OUTPUT_DIR = '/Users/mike/Projects/josephson.org/htdocs/includes'
 
 # map of source name to (file name, generating method, method args)
 SOURCES = {
-    'pinboard' : ('pinboard.html', 'get_pinboard', 'PINBOARD_API_KEY'),
-    'last.fm' : ('last_fm.html', 'get_last_fm', 'LASTFM_USERNAME', 'LASTFM_KEY'),
-    'goodreads' : ('goodreads.html', 'get_goodreads', 'GOODREADS_USERID', 'currently-reading'),
-    'lanyrd' : ('lanyrd.html', 'get_lanyrd', 'LANYRD_USERNAME'),
-    'so' : ('so.html', 'get_so_answers', 'STACKOVERFLOW_USERID'),
-    'github' : ('github.html', 'get_github_activity', 'GITHUB_USERNAME')
+    'pinboard' : ('pinboard.html', 'bookmarks.get_pinboard', 'PINBOARD_API_KEY'),
+    'last.fm' : ('last_fm.html', 'last_fm.get_last_fm', 'LASTFM_USERNAME', 'LASTFM_KEY'),
+    'goodreads' : ('goodreads.html', 'goodreads.get_goodreads', 'GOODREADS_USERID', 'currently-reading'),
+    'lanyrd' : ('lanyrd.html', 'lanyrd.get_lanyrd', 'LANYRD_USERNAME'),
+    'so' : ('so.html', 'stackoverflow.get_so_answers', 'STACKOVERFLOW_USERID'),
+    'github' : ('github.html', 'github.get_github_activity', 'GITHUB_USERNAME')
 }
 
 HEALTH_CHECK_URL=None
